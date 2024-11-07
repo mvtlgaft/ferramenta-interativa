0809ed5a431ebd1942f96b21208d1a8b900b3157
mvtlgaft <m2102d3@gmail.com>
2024-11-07 07:50:33 -0300

    Add files via upload

---
+++ marcos flahcard/flashcard-aula_10/perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Programação',
+    'O que é jogo',
+    'atividade cuja natureza ou finalidade é a diversão, o entretenimento'
+)
+
+criaCartao(
+    'jogo terror',
+    'qual é os tipos de jogos de terror',
+    'Terror de ação. ...Terror psicológico. ...Terror de susto. ...Terror reverso.'
+)
+
+criaCartao(
+    'filme',
+    'O que é filme de cinema',
+    'sequência de imagens registradas em filme cinematográfico, fotográfico ou videoteipe, para exibição em movimento ou não; fita, película.'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz bom dia em Inglês?',
+    'good morning'
+)
